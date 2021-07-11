9b9652e04a238cfdc3c4c8cbe3874b51ae785f7a
Mateus V. Farias <mateus_vinicius15@outlook.com>
2021-07-10 23:10:58 -0400

    chore: add gray, title and text colors

--- src/styles/theme/index.js
+++ src/styles/theme/index.js
@@ -1,8 +1,11 @@
 export const theme = {
   background: '#eeeeee',
   white: '#ffffff',
-  black: '#000000',
+  black: '#2e384d',
   emerald: '#51d764',
   gray: 'rgb(115, 115, 128)',
   alto: '#dbdbdb',
+  text: '#666666',
+  title: '#2e384d',
+  'gray-100': '#e6e8eb',
 };
