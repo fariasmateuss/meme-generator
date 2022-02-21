80225a0ceb66a6a2938cd4c0fa572bc19e5c7413
Mateus V. Farias <mateus_vinicius15@outlook.com>
2022-02-20 22:04:28 -0400

    feat: add favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -47,7 +47,75 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Luckiest+Guy&family=Roboto:wght@400;500;700&display=swap"
             rel="stylesheet"
           />
+
+          <link
+            rel="shortcut icon"
+            type="image/x-icon"
+            sizes="16x16 32x32 64x64 96x96"
+            href="/favicons/favicon.ico"
+          />
+          <link
+            rel="shortcut icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicons/favicon-16x16.png"
+          />
+          <link
+            rel="shortcut icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicons/favicon-32x32.png"
+          />
+          <link
+            rel="shortcut icon"
+            type="image/png"
+            sizes="64x64"
+            href="/favicons/favicon-64x64.png"
+          />
+          <link
+            rel="shortcut icon"
+            type="image/png"
+            sizes="96x96"
+            href="/favicons/favicon-96x96.png"
+          />
+          <link
+            rel="shortcut icon"
+            type="image/png"
+            sizes="128x128"
+            href="/favicons/favicon-128x128.png"
+          />
+          <link
+            rel="shortcut icon"
+            type="image/png"
+            sizes="256x256"
+            href="/favicons/favicon-256x256.png"
+          />
+          <link
+            rel="shortcut icon"
+            type="image/png"
+            sizes="512x512"
+            href="/favicons/favicon-512x512.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            type="image/png"
+            sizes="152x152"
+            href="/favicons/apple-touch-icon-152x152.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            type="image/png"
+            sizes="167x167"
+            href="/favicons/apple-touch-icon-167x167.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            type="image/png"
+            sizes="180x180"
+            href="/favicons/apple-touch-icon-180x180.png"
+          />
         </Head>
+
         <body>
           <Main />
           <NextScript />
