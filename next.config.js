8b3be3a757422555a5c33623b2a7d59ed529a4d8
Mateus V. Farias <mateus_vinicius15@outlook.com>
2022-03-24 08:58:08 -0400

    build: configure file loader to handle .mp3

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 module.exports = {
+  webpack: config => {
+    config.module.rules.push({
+      test: /\.(mp3)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          publicPath: '/_next/static/sounds/',
+          outputPath: 'static/sounds/',
+          name: '[name].[ext]',
+          esModule: false,
+        },
+      },
+    });
+
+    return config;
+  },
   images: {
     domains: ['i.imgflip.com'],
   },
