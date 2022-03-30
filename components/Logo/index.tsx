ab71a12e321ee2f2044ba57c0e6d2935b5aa77e6
Mateus V. Farias <mateus_vinicius15@outlook.com>
2022-03-30 19:04:54 -0400

    feat: display blur placeholder while the image is loaded

--- components/Logo/index.tsx
+++ components/Logo/index.tsx
@@ -7,24 +7,30 @@ export function Logo() {
   const { mode } = useThemeState();
   const { t } = useI18nState();
 
-  return mode === 'light' ? (
+  const isDarkMode = mode === 'dark';
+
+  return isDarkMode ? (
     <Image
-      src="/static/light-logo.svg"
+      src="/static/dark-logo.svg"
       alt={t.heading.meme_generator_description}
       aria-label={t.heading.meme_generator_description}
       width={383}
       height={155}
       objectFit="contain"
+      placeholder="blur"
+      blurDataURL="/static/dark-logo.svg"
       priority
     />
   ) : (
     <Image
-      src="/static/dark-logo.svg"
+      src="/static/light-logo.svg"
       alt={t.heading.meme_generator_description}
       area-label={t.heading.meme_generator_description}
       width={383}
       height={155}
       objectFit="contain"
+      placeholder="blur"
+      blurDataURL="/static/light-logo.svg"
       priority
     />
   );
