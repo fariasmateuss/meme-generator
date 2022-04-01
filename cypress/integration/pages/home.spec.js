4b02855def4afd0898c2d585cf393fd7d687c1ff
Mateus V. Farias <mateus_vinicius15@outlook.com>
2022-03-31 21:11:51 -0400

    test: should load the home page and show content

--- cypress/integration/pages/home.spec.js
+++ cypress/integration/pages/home.spec.js
@@ -1,7 +1,8 @@
 /// <reference types="cypress" />
 
 context('Home page', () => {
-  it('should load the home page', () => {
+  it('should load the home page and show content', () => {
     cy.visit('/');
+    cy.contains('Pick a meme');
   });
 });
