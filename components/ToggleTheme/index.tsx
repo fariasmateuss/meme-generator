030d357981933456d36233cd5e2e17c3323adcc5
Mateus V. Farias <mateus_vinicius15@outlook.com>
2022-05-05 23:47:06 -0400

    feat: add titleattribute to toggle theme button

--- components/ToggleTheme/index.tsx
+++ components/ToggleTheme/index.tsx
@@ -1,29 +1,36 @@
-import { useCallback, useMemo } from 'react';
+import { useMemo } from 'react';
 import useSound from 'use-sound';
 import { MdLightMode, MdModeNight } from 'react-icons/md';
 import { IconContext } from 'react-icons';
 
 import { useThemeDispatch, useThemeState } from 'contexts/theme/ThemeContext';
+import { useI18nState } from 'contexts/i18n/I18Context';
 import switchOnSound from 'public/sounds/switch-on.mp3';
 import switchOffSound from 'public/sounds/switch-off.mp3';
 
 import * as S from 'styles/components/ToggleTheme';
 
 export function ToggleTheme() {
-  const { mode } = useThemeState();
   const { onSelectMode } = useThemeDispatch();
+  const { mode } = useThemeState();
+  const { t } = useI18nState();
 
   const isDarkMode = mode === 'dark';
-  const [play] = useSound(isDarkMode ? switchOnSound : switchOffSound);
-  const iconTitle = isDarkMode ? 'Switch to light mode' : 'Switch to dark mode';
+
+  const iconTitle = isDarkMode
+    ? t.actions.theme.activate_light_mode
+    : t.actions.theme.activate_dark_mode;
+
   const iconColor = isDarkMode
     ? 'var(--night-mode-color)'
     : 'var(--light-mode-color)';
 
-  const handleClick = useCallback(() => {
+  const [play] = useSound(isDarkMode ? switchOnSound : switchOffSound);
+
+  const handleClick = () => {
     onSelectMode(mode);
     play();
-  }, [mode, play]);
+  };
 
   const iconContextProviderValue = useMemo(
     () => ({
@@ -34,7 +41,7 @@ export function ToggleTheme() {
   );
 
   return (
-    <S.Button onClick={handleClick} aria-label={iconTitle}>
+    <S.Button onClick={handleClick} title={iconTitle} aria-label={iconTitle}>
       <IconContext.Provider value={iconContextProviderValue}>
         {isDarkMode ? <MdModeNight /> : <MdLightMode />}
       </IconContext.Provider>
